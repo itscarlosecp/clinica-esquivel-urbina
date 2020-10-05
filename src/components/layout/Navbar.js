cbfeb22d8dbffb49e1dc34763fcbd2ad45d2af6a
Carlos Castillo <carloseduardocp.01@gmail.com>
2020-10-05 12:35:02 -0300

    Navbar href attribute wrong

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import Logo from '../../img/logo-texto.png';
-import '../../css/Navbar.css';
-import LanguageBtn from './LanguageBtn';
+import React from 'react'
+import Logo from '../../img/logo-texto.png'
+import '../../css/Navbar.css'
+import LanguageBtn from './LanguageBtn'
 
-function Navbar({lang, changeLang}) {
+function Navbar({ lang, changeLang }) {
 	return (
 		<div className='navbar-container'>
 			<nav className='navbar navbar-green bg-green'>
-				<a className='navbar-brand' href='#'>
+				<a className='navbar-brand' href='/'>
 					<img src={Logo} height='30' alt='' />
 				</a>
 				<LanguageBtn lang={lang} changeLang={changeLang} />
 			</nav>
 		</div>
-	);
-};
+	)
+}
 
-export default Navbar;
+export default Navbar
