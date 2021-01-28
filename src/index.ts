1883096ac89be582e3516f9f1c30b4cf621ff3e6
Carlos Castillo <carloseduardocp.01@gmail.com>
2021-01-28 19:07:32 -0300

    Redoing everything

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ class Navbar {
 		this.setTransparency();
 	}
 
-	setTransparency() {
+	private setTransparency() {
 		window.addEventListener("scroll", (_e: Event) => {
 			if (window.scrollY > this.transPosition) {
 				this.component.classList.remove("bg-white");
@@ -20,4 +20,32 @@ class Navbar {
 	}
 }
 
+class Banner {
+	private readonly element: HTMLElement;
+	private descriptionContainer: HTMLDivElement;
+	public description: string;
+
+	constructor(desc: string) {
+		this.element = document.getElementById("banner")! as HTMLElement;
+		this.descriptionContainer = this.element.querySelector(
+			"#description"
+		) as HTMLDivElement;
+
+		this.description = desc;
+		this.renderDescription();
+	}
+
+	private renderDescription() {
+		const paragraph = this.descriptionContainer.querySelector(
+			"p"
+		)! as HTMLParagraphElement;
+		paragraph.innerText = this.description;
+	}
+}
+
 const navbar = new Navbar();
+
+const msg =
+	"Ofrecemos la más alta calidad en prótesis fijas y removibles, implantes dentales, endodoncias, coronas libres de metal. Nos preocupamos por la función y estética bucal.";
+
+const header = new Banner(msg);
